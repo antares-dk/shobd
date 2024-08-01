d769e53fa8f45b97c979f8676a401cf20f7983bf
antares-dk <m_antares@msn.com>
2024-08-01 08:53:34 +0200

    Create serie.h

---
+++ src/serie.h
@@ -0,0 +1,13 @@
+//
+// AntareS
+// Fichier : serie.cpp / serie.h
+// Version : 0.0.1
+// Date : Juillet 2024
+//
+
+#define USB_Speed 115200
+
+void USB_Init();
+void USB_Read(int *);
+void USB_Extract(int *);
+void USB_Sep(int *, char *);
